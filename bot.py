4223d9ce3a8b9dd0cbc6cac5c719a1333ec3a2fd
Rouven <seifertrouven@gmail.com>
2021-04-20 13:14:22 +0200

    improved bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,19 @@
 import os
-import discord
 import sqlite3
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv('./.env')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-if __name__ == "__main__":
+def main():
     con = sqlite3.connect('players.db')
     cur = con.cursor()
-    
     cur.execute("SELECT * FROM players")
     registered_players = cur.fetchall()
-    
-    bot = commands.Bot(command_prefix="t.", help_command=None, case_insensitive = True)
-    
+    bot = commands.Bot(command_prefix="t.", help_command=None, case_insensitive=True)
+
     @bot.command()
     async def register(ctx):
         if not user_registered(ctx.author):
@@ -24,34 +22,39 @@ if __name__ == "__main__":
             con.commit()
             await ctx.channel.send("Welcome to the Truckers, {}".format(ctx.author.mention))
         else:
-            await ctx.channel.send("You are already registered") 
-    
+            await ctx.channel.send("You are already registered")
+ 
     @bot.command()
     async def profile(ctx, *args):
-        if user_registered(ctx.author): 
+        #TODO add mention profile
+        if user_registered(ctx.author):
             player = get_player(ctx.author.id)
             profile = discord.Embed(title="{}'s Profile".format(player[1]))
             profile.add_field(name="Money", value=player[3])
             await ctx.channel.send(embed=profile)
         else:
             await ctx.channel.send("{} you are not registered yet! Try `t.register` to get started".format(ctx.author.mention))
-    
+ 
     @bot.command()
-    async def drive(ctx, *args):
-        drive_embed = discord.Embed(title="{} is driving".format(ctx.author.name), description="We hope he has fun")
+    async def drive(ctx):
+        drive_embed = discord.Embed(title="{} is driving".format(ctx.author.name),
+                                    description="We hope he has fun")
         await ctx.channel.send(embed=drive_embed)
-    
-    
+
     def user_registered(user):
         for player in registered_players:
             if user.id == player[0]:
                 return True
         return False
-    
-    # TODO add player as class 
+ 
     def get_player(player_id):
+        # TODO add player as class 
         for player in registered_players:
             if player[0] == player_id:
                 return player
         return None
+
     bot.run(BOT_TOKEN)
+
+if __name__ == '__main__':
+    main()
