1b0687a3daeb1077f11977f3031c652abc911e69
Rouven <seifertrouven@gmail.com>
2021-07-31 18:21:13 +0200

    added more slash commands and fixed the subcommands

--- commands/gambling.py
+++ commands/gambling.py
@@ -6,6 +6,7 @@ import discord
 
 from discord.ext import commands
 from discord_slash import cog_ext
+from discord_slash.utils.manage_commands import create_option
 
 import players
 import places
@@ -21,7 +22,20 @@ class Gambling(commands.Cog):
         super().__init__()
 
     # @commands.command(aliases=["cf"])
-    @cog_ext.cog_slash(guild_ids=[830928381100556338])
+    @cog_ext.cog_slash(guild_ids=[830928381100556338],
+            options=[
+                create_option(
+                    name="side", 
+                    description="The side you bet on",
+                    option_type=3,
+                    choices=["heads", "tails"],
+                    required=True),
+                create_option(
+                    name="amount", 
+                    description="The amount you bet",
+                    option_type=10,
+                    required=True)
+            ])
     @commands.bot_has_permissions(view_channel=True, send_messages=True,
                                   embed_links=True, attach_files=True, read_message_history=True,
                                   use_external_emojis=True)
@@ -32,35 +46,17 @@ class Gambling(commands.Cog):
         player = await players.get(ctx.author.id)
         if "coinflip" not in places.get(player.position).commands:
             raise places.WrongPlaceError("We are not in Las Vegas!!!")
-        try:
-            if amount == "all":
-                amount = player.money
-            elif amount == "half":
-                amount = round(player.money / 2)
-            else:
-                amount = int(amount)
+        await players.debit_money(player, amount)
+        if randint(0, 1) == 0:
+            result = "heads"
+        else:
+            result = "tails"
 
-            if str.lower(side) not in ["h", "head", "t", "tails"]:
-                await ctx.send("**Syntax:** `t.coinflip [h/t] <amount>`")
-                return
-
-            if side == "h":
-                side = "heads"
-            elif side == "t":
-                side = "tails"
-            await players.debit_money(player, amount)
-            if randint(0, 1) == 0:
-                result = "heads"
-            else:
-                result = "tails"
-
-            if result == side:
-                await ctx.send("Congratulations, it was {}. You won ${}".format(result, "{:,}".format(amount)))
-                await players.add_money(player, amount*2)
-            else:
-                await ctx.send("Nope, it was {}. You lost ${}".format(result, "{:,}".format(amount)))
-        except (TypeError, ValueError):
-            await ctx.send("**Syntax:** `t.coinflip [h/t] <amount>`")
+        if result == side:
+            await ctx.send("Congratulations, it was {}. You won ${}".format(result, "{:,}".format(amount)))
+            await players.add_money(player, amount*2)
+        else:
+            await ctx.send("Nope, it was {}. You lost ${}".format(result, "{:,}".format(amount)))
 
     # @commands.command()
     @cog_ext.cog_slash(guild_ids=[830928381100556338])
@@ -75,35 +71,25 @@ class Gambling(commands.Cog):
         player = await players.get(ctx.author.id)
         if "slots" not in places.get(player.position).commands:
             raise places.WrongPlaceError("We are not in Las Vegas!!!")
-        try:
-            if amount == "all":
-                amount = player.money
-            elif amount == "half":
-                amount = round(player.money / 2)
-            else:
-                amount = int(amount)
-            await players.debit_money(player, amount)
+        await players.debit_money(player, amount)
 
-            chosen_items = choices(sample(items.get_all(), 8), k=3)
-            machine = "<|"
-            for item in chosen_items:
-                machine += str(self.bot.get_emoji(item.emoji))
-                machine += "|"
-            machine += ">"
+        chosen_items = choices(sample(items.get_all(), 8), k=3)
+        machine = "<|"
+        for item in chosen_items:
+            machine += str(self.bot.get_emoji(item.emoji))
+            machine += "|"
+        machine += ">"
 
-            slots_embed = discord.Embed(description=machine, colour=discord.Colour.gold())
-            slots_embed.set_author(name=f"{ctx.author.name}'s slots", icon_url=ctx.author.avatar_url)
+        slots_embed = discord.Embed(description=machine, colour=discord.Colour.gold())
+        slots_embed.set_author(name=f"{ctx.author.name}'s slots", icon_url=ctx.author.avatar_url)
 
-            if chosen_items.count(chosen_items[0]) == 3:
-                slots_embed.add_field(name="Result", value=":tada: Congratulations, you won {:,} :tada:".format(amount*10))
-                await players.add_money(player, amount*11)
-            elif chosen_items.count(chosen_items[0]) == 2 or chosen_items.count(chosen_items[1]) == 2: 
-                slots_embed.add_field(name="Result", value="You won ${:,}".format(amount))
-                await players.add_money(player, amount*2)
-            else:
-                slots_embed.add_field(name="Result", value="You lost ${:,}".format(amount))
+        if chosen_items.count(chosen_items[0]) == 3:
+            slots_embed.add_field(name="Result", value=":tada: Congratulations, you won {:,} :tada:".format(amount*10))
+            await players.add_money(player, amount*11)
+        elif chosen_items.count(chosen_items[0]) == 2 or chosen_items.count(chosen_items[1]) == 2: 
+            slots_embed.add_field(name="Result", value="You won ${:,}".format(amount))
+            await players.add_money(player, amount*2)
+        else:
+            slots_embed.add_field(name="Result", value="You lost ${:,}".format(amount))
 
-            await ctx.send(embed=slots_embed)
-
-        except (TypeError, ValueError) as e:
-            await ctx.send("**Syntax:** `t.slots <amount>`")
+        await ctx.send(embed=slots_embed)
