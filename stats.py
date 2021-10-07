fae1d45a972119b78584e8ae2c24ca897c652a08
Rouven <seifertrouven@gmail.com>
2021-10-07 18:23:05 +0200

    removed profile delete command

--- stats.py
+++ stats.py
@@ -6,12 +6,6 @@ from discord.ext import commands
 from discord_slash import cog_ext
 from discord_slash.context import MenuContext
 from discord_slash.utils.manage_commands import create_option
-from discord_slash.utils.manage_components import (
-    create_button,
-    create_actionrow,
-    ComponentContext,
-)
-import api.jobs as jobs
 import api.players as players
 import api.trucks as trucks
 import api.levels as levels
@@ -82,37 +76,6 @@ class Stats(commands.Cog):
         profile_embed.add_field(name="Current truck", value=truck.name)
         profile_embed.set_image(url=truck.image_url)
         return profile_embed
-
-    @cog_ext.cog_subcommand(base="support", subcommand_group="profile", guild_ids=[839580174282260510])
-    async def delete(self, ctx) -> None:
-        """
-        Delete your account
-        """
-        await players.get(ctx.author.id)
-        await ctx.send(
-            "Are you sure you want to delete your profile? **All your ingame stats will be lost!**",
-            hidden=True,
-            components=[
-                create_actionrow(
-                    create_button(style=3, label="Yes", custom_id="confirm_deletion"),
-                    create_button(style=4, label="No", custom_id="abort_deletion"),
-                )
-            ],
-        )
-
-    @cog_ext.cog_component()
-    async def confirm_deletion(self, ctx: ComponentContext):
-        player = await players.get(ctx.author.id)
-        await players.remove(player)
-        job = jobs.get(ctx.author.id)
-        if job is not None:
-            jobs.remove(job)
-        await ctx.edit_origin(components=[])
-        await ctx.send("Your profile got deleted. We will miss you :(", hidden=True)
-
-    @cog_ext.cog_component()
-    async def abort_deletion(self, ctx: ComponentContext):
-        await ctx.edit_origin(content="Deletion aborted", components=[])
 
     @cog_ext.cog_slash(
         options=[
