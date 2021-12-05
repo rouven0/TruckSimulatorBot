c3dd994dc950ee53080a849cf8bcb26539760c95
Rouven <seifertrouven@gmail.com>
2021-12-05 12:25:03 +0100

    released company commands

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 MAP_BORDER = 25
 LOG_FORMAT = "%(levelname)s [%(module)s.%(funcName)s]: %(message)s"
-EXTENSIONS = ["driving", "economy", "gambling", "guide", "misc", "stats", "system", "truck"]
+EXTENSIONS = ["companies", "driving", "economy", "gambling", "guide", "misc", "stats", "system", "truck"]
