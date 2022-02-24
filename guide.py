ed57569bcb02a93b78193ff3f99a090fb5ec5357
Rouven <seifertrouven@gmail.com>
2022-02-24 15:56:24 +0100

    added a topic select to the guide

--- guide.py
+++ guide.py
@@ -1,6 +1,7 @@
 # pylint: disable=unused-argument
 from os import listdir
 from flask_discord_interactions import DiscordInteractionsBlueprint, Message, Embed
+from flask_discord_interactions.models.component import ActionRow, SelectMenu, SelectMenuOption
 from flask_discord_interactions.models.option import CommandOptionType
 from flask_discord_interactions.models.embed import Field, Author, Media
 from flask_discord_interactions.models.autocomplete import Autocomplete
@@ -83,6 +84,17 @@ def place_autocomplete(ctx, place):
 )
 def guide(ctx, topic: str = "introduction") -> Message:
     """A nice little guide that helps you understand this bot."""
+    return Message(embed=get_guide_embed(topic), components=[get_guide_select()])
+
+
+@guide_bp.custom_handler(custom_id="guide_topic")
+def guide_topic(ctx):
+    """Handler for the topic select"""
+    return Message(embed=get_guide_embed(ctx.values[0]), components=[get_guide_select()], update=True)
+
+
+def get_guide_embed(topic: str) -> Embed:
+    """Returns the fitting guide embed for a topic"""
     with open(f"./guide/{topic}.md", "r", encoding="utf8") as guide_file:
         topic = str.lower(topic)
         image_url = guide_file.readline()
@@ -96,4 +108,20 @@ def guide(ctx, topic: str = "introduction") -> Message:
             guide_embed.image = Media(url=image_url)
         else:
             guide_embed.description = image_url + guide_embed.description
-    return Message(embed=guide_embed)
+        return guide_embed
+
+
+def get_guide_select() -> ActionRow:
+    """Builder for the topic select"""
+    return ActionRow(
+        components=[
+            SelectMenu(
+                custom_id="guide_topic",
+                options=[
+                    SelectMenuOption(label=str.upper(f[:1]) + f[1 : f.find(".")], value=f[: f.find(".")])
+                    for f in listdir("./guide")
+                ],
+                placeholder="Select a topic",
+            )
+        ]
+    )
