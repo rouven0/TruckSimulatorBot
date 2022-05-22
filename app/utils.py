2cea54d497c186e0b60d5dc11ff46f9ff55fa477
Rouven <seifertrouven@gmail.com>
2022-05-22 21:51:20 +0200

    added get_localizations()

--- app/utils.py
+++ app/utils.py
@@ -4,6 +4,7 @@ Some utility functions
 
 import logging
 
+import i18n
 from flask_discord_interactions.context import Context
 
 
@@ -24,3 +25,13 @@ def commatize(i: int) -> str:
         1000000 -> 1,000,000
     """
     return f"{i:,}"
+
+
+def get_localizations(key: str) -> dict:
+    """
+    Returns all localizations for a string
+    """
+    localizations = {}
+    for locale in i18n.get("available_locales"):
+        localizations[locale] = i18n.t(key, locale=locale)
+    return localizations
