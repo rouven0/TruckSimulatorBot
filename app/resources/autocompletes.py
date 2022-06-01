aab0b86848dcd3f4f0f83892f23281549df3041a
Rouven <seifertrouven@gmail.com>
2022-06-01 09:10:41 +0200

    localized autocompletes

--- app/resources/autocompletes.py
+++ app/resources/autocompletes.py
@@ -2,11 +2,15 @@
 Some nice autocomplete handlers that are used everywhere
 """
 
+from i18n import set as set_i18n
+from i18n import t
+from flask_discord_interactions.models.option import Choice
 from resources import players
 
 
 def amount_all(ctx, *args):
     """Some enhancements for int options"""
+    set_i18n("locale", ctx.locale)
     player = players.get(ctx.author.id)
     amount = None
     for option in args:
@@ -16,14 +20,14 @@ def amount_all(ctx, *args):
     if not amount:
         return []
     if len(amount.value) == 0 or amount.value in "all":
-        lst.append({"name": "all", "value": player.money})
+        lst.append(Choice(name=t("autocomplete.all"), value=player.money))
     if len(amount.value) == 0 or amount.value in "half":
-        lst.append({"name": "half", "value": round(player.money / 2)})
+        lst.append(Choice(name=t("autocomplete.half"), value=round(player.money / 2)))
     if amount.value.isnumeric() and player.money >= int(amount.value):
-        lst.append({"name": amount.value, "value": int(amount.value)})
+        lst.append(Choice(name=amount.value, value=int(amount.value)))
     if amount.value.isnumeric() and player.money >= int(amount.value) * 1000:
-        lst.append({"name": f"{amount.value} thousand", "value": int(amount.value) * 1000})
+        lst.append(Choice(name=t("autocomplete.thousand", amount=amount.value), value=int(amount.value) * 1000))
     if amount.value.isnumeric() and player.money >= int(amount.value) * 1000000:
-        lst.append({"name": f"{amount.value} million", "value": int(amount.value) * 1000000})
+        lst.append(Choice(name=t("autocomplete.million", amount=amount.value), value=int(amount.value) * 1000000))
 
     return lst
