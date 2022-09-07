05aad456501838f67a093d13ba7524f4054ffaf6
Rouven <rouven@rfive.de>
2022-09-07 16:22:33 +0200

    updated the logo

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ from i18n import t
 MAP_BORDER = 25
 LOG_FORMAT = "%(levelname)s [%(module)s.%(funcName)s]: %(message)s"
 EMBED_COLOR = int("0x2f3136", 16)
-SELF_AVATAR_URL = "https://cdn.discordapp.com/avatars/831052837353816066/c9b904f935580ac68f54f184f6fc620c.png"
+SELF_AVATAR_URL = "https://images.trucksimulatorbot.rfive.de/logo"
 BASE_IMAGE = 0
 IMAGE_HOST = "https://images.trucksimulatorbot.rfive.de"
 
