2b45ac7d26cc20f1c32269e72935d6edb0e46d02
Rouven Seifert <rouven@rfive.de>
2024-03-25 20:02:47 +0100

    gunicorn: use sockets

--- module.nix
+++ module.nix
@@ -2,12 +2,19 @@ inputs: { lib, pkgs, config, ... }:
 with lib;
 let
   cfg = config.services.trucksimulatorbot;
+  docs = pkgs.callPackage ./docs.nix { };
   appEnv = pkgs.python311.withPackages (p: with p; [ gunicorn (pkgs.python311Packages.callPackage ./default.nix { }) ]);
   imageEnv = pkgs.python311.withPackages (p: with p; [ gunicorn inputs.images.packages.x86_64-linux.default ]);
 in
 {
   options.services.trucksimulatorbot = {
     enable = mkEnableOption "Trucksimulatorbot";
+    domain = mkOption {
+      type = types.str;
+      description = mdDoc ''
+        Domain name the app runs under.
+      '';
+    };
     listenPort = mkOption {
       type = types.port;
       default = 9000;
@@ -62,10 +69,15 @@ in
   };
 
   config = mkIf (cfg.enable) {
+    systemd.sockets.trucksimulator = {
+      wantedBy = [ "sockets.target" ];
+      before = [ "nginx.service" ];
+      socketConfig.ListenStream = "/run/trucksimulator/app.sock";
+    };
     systemd.services.trucksimulator = {
       enable = true;
       after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
+      requires = [ "trucksimulator.socket" ];
       environment = {
         DISCORD_CLIENT_ID = cfg.discord.clientId;
         DISCORD_PUBLIC_KEY = cfg.discord.publicKey;
@@ -76,16 +88,35 @@ in
       };
       serviceConfig = {
         DynamicUser = true;
-        ExecStart = "${appEnv}/bin/gunicorn trucksimulator:app -b 0.0.0.0:${toString cfg.listenPort} --error-logfile -";
+        ExecStart = "${appEnv}/bin/gunicorn trucksimulator:app -b /run/trucksimulator/app.sock --error-logfile -";
       };
+    };
+    systemd.sockets.trucksimulator-images = {
+      wantedBy = [ "sockets.target" ];
+      before = [ "nginx.service" ];
+      socketConfig.ListenStream = "/run/trucksimulator/app.sock";
     };
     systemd.services.trucksimulator-images = {
       enable = true;
       after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
+      requires = [ "trucksimulator-images.socket" ];
       serviceConfig = {
         DynamicUser = true;
-        ExecStart = "${imageEnv}/bin/gunicorn trucksimulatorbot-images:app -b 0.0.0.0:${toString cfg.images.listenPort} --error-logfile -";
+        ExecStart = "${imageEnv}/bin/gunicorn trucksimulatorbot-images:app -b /run/trucksimulator/images.sock --error-logfile -";
+      };
+    };
+    services.nginx.virtualHosts = {
+      "${cfg.domain}" = {
+        locations."/invite".return = "301 https://discord.com/api/oauth2/authorize?client_id=831052837353816066&permissions=262144&scope=bot%20applications.commands";
+        locations."/" = {
+          proxyPass = "http://unix:/run/trucksimulator/app.sock";
+        };
+        locations."/images/" = {
+          proxyPass = "http://unix:/run/trucksimulator/images.sock";
+        };
+        locations."/docs" = {
+          root = "${docs}";
+        };
       };
     };
   };
